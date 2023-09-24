1ab24938e3b8e596ac267a438044e6fba408e946
kenzi1111 <88967765+kenzi1111@users.noreply.github.com>
2023-09-24 17:55:30 +0900

    Add Bmi.py

--- bmi.py
+++ bmi.py
@@ -1,16 +1,14 @@
-class Adult:
-    def __init__(self,height,weight,bmi):
+class BMI:
+    def __init__(self, height, weight):
         self.height = height
         self.weight = weight
-        self.bmi = weight/height**2
 
-    def rule(self):
-        if not (10 <= self.bmi <=40):
-             raise ValueError(f"サイズオーバーです")
-        
-        bmi = round(self.bmi,2)
+    def calculate_bmi(self):
+        return self.weight / (self.height**2)
 
 
-tanaka_bmi = Adult(height=180.0, weight=67.0)
-    
-print(tanaka_bmi)
+tanaka_bmi = BMI(height=1.80, weight=67.0)
+
+result = round(tanaka_bmi.calculate_bmi(),2)
+
+print(result)
